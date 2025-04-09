0fc90abeac95283b26e72ad20c3cffc17a1d2467
Daria Pavlyuk <pavlyuk.daria81@gmail.com>
2025-04-08 22:04:56 -0400

    Project Navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,20 +3,17 @@ import { Link } from 'react-router-dom';
 import logo from '../image/Code.png';
 
 
-
-
 const Header = () => {
   return (
     <header className="w-full px-6 py-4 flex justify-between items-center border-b border-gray-300 bg-white">
 
       {/* Logo */}
       <div className="flex items-center space-x-2">
-        <img src={logo} alt="CodeCrew Logo" className="w-8 h-8" />
-        <div className="text-2xl font-bold">CodeCrew</div>
+        <Link to="/">
+          <img src={logo} alt="CodeCrew Logo" className="w-8 h-8" />
+        </Link>
+        <Link to="/" className="text-2xl font-bold">CodeCrew</Link>
       </div>
-
-
-
       {/* Nav */}
       <nav className="flex items-center gap-4">
         <Link to="/signin" className="px-4 py-2 border rounded-md text-gray-700 hover:bg-gray-100">
---
+++ src/pages/BrowseProjects.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BrowseProjects = () => {
+  return (
+    <div>
+      <h1>Browse Projects</h1>
+      <p> Find and Join Exciting Projects!</p>
+    </div>
+  );
+};
+
+export default BrowseProjects;
