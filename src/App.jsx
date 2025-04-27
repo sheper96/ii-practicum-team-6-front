65fd371d24f20bedde7e2c1e6eec6d8499740e07
brrr123 <65033667+brrr123@users.noreply.github.com>
2025-04-27 13:00:03 -0600

    Added a new "Submit Project" page html. Updated the routing with "/projects/new".
    Signed-off-by: brrr123 <65033667+brrr123@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { getAllData } from './util/index';
+import React, {useState, useEffect} from 'react';
+import {Routes, Route} from 'react-router-dom';
+import {getAllData} from './util/index';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import Register from './pages/Register';
@@ -9,8 +9,9 @@ import ProjectDetails from './pages/ProjectDetails';
 import CodeCrewLayout from './layouts/CodeCrewLayout';
 import ForgotPassword from './pages/ForgotPassword';
 import EditProfile from './pages/EditProfile';
-import { UserProvider } from './components/UserContext';
+import {UserProvider} from './components/UserContext';
 import Profile from './pages/Profile';
+import SubmitProject from './pages/SubmitProject.jsx';
 
 
 const URL = 'http://localhost:8000/api/v1/';
@@ -33,21 +34,22 @@ const App = () => {
   }, []);
 
   return (
-    <UserProvider>
-      <CodeCrewLayout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/projects" element={<BrowseProjects />} />
-          <Route path="/projects/:id" element={<ProjectDetails />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/edit-profile" element={<EditProfile />} />
-          <Route path="/browse-projects" element={<BrowseProjects />} />
-          <Route path="/my-profile" element={<Profile />} />
-        </Routes>
-      </CodeCrewLayout>
-    </UserProvider>
+      <UserProvider>
+        <CodeCrewLayout>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/signin" element={<SignIn/>}/>
+            <Route path="/register" element={<Register/>}/>
+            <Route path="/projects" element={<BrowseProjects/>}/>
+            <Route path="/projects/new" element={<SubmitProject/>}/>
+            <Route path="/projects/:id" element={<ProjectDetails/>}/>
+            <Route path="/forgot-password" element={<ForgotPassword/>}/>
+            <Route path="/edit-profile" element={<EditProfile/>}/>
+            <Route path="/browse-projects" element={<BrowseProjects/>}/>
+            <Route path="/my-profile" element={<Profile/>}/>
+          </Routes>
+        </CodeCrewLayout>
+      </UserProvider>
   );
 };
 
