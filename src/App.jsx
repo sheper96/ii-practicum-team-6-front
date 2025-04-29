eb435a40d39b4592aba438fc4a960f62f521b6f6
Daria Pavlyuk <pavlyuk.daria81@gmail.com>
2025-04-29 16:53:36 -0400

    Logout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
-//import { getAllData } from './util/index';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import Register from './pages/Register';
